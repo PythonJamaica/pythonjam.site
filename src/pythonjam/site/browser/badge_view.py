8f71b1d9950b006bf391421c7bb81396de37c07b
Mumba Sambo <mumbasambo@live.com>
2015-11-19 14:07:32 +0000

    Updated badge view method to return JSON.

--- src/pythonjam/site/browser/badge_view.py
+++ src/pythonjam/site/browser/badge_view.py
@@ -1,8 +1,27 @@
 from Products.Five import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+import json
 
 
 class BadgeView(BrowserView):
 
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
     def __call__(self):
-        """"""
-        return "this is where the badge stuff shows up"
+        """Method to return the Badge Class as JSON"""
+        self.context.id
+        url = self.context.absolute_url()
+        image = "%s/@@images/image/" % url
+        name = self.context.title
+        description = self.context.description
+        criteria = self.context.criteria
+        issuer = "%s/@@openbadge_issuer.json" % self.request['BASE1']
+        badge = json.dumps({'name': name,
+                            'criteria': criteria,
+                            'issuer': issuer,
+                            'image': image,
+                            'description': description})
+        return badge
